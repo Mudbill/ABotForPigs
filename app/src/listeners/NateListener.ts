50d617e776546f735d842ae95f23bcb97393b943
Mudbill <mudbill.b@gmail.com>
2021-02-21 21:04:51 +0100

    Fixed 60 sec time limit for db emojis

--- app/src/listeners/NateListener.ts
+++ app/src/listeners/NateListener.ts
@@ -1,16 +1,13 @@
-import { Client } from "discord.js";
+import { Client, Message } from "discord.js";
 import { console } from '../util/log';
-// import crypto from 'crypto';
-// import https from 'https';
-// import child_process from 'child_process';
-// import fs from 'fs';
 import db from '../util/db';
 
-let lastTime: Date;
+const mud = '105692860271042560';
+const nate = '203652305772478465';
+
 
 export default async function (client: Client) {
 	client.on('message', async (msg) => {
-		const nate = '203652305772478465';
 		if (msg.member.user.id !== nate) return;
 
 		const emojis = await db.findOne({ _id: 'nate_emojis' });
@@ -19,12 +16,12 @@ export default async function (client: Client) {
 			const msgEmojis = msg.content.matchAll(/(?<!\\)<:\w*:([0-9]{18})>/g);
 
 			for (const emoji of msgEmojis) {
-				const [, emojiID] = emoji;
+				const emojiID = emoji[1];
 
 				if (list.includes(emojiID)) {
 					const status = await db.findOne({ _id: 'nate_enable' });
 					if (status && status.value === true) {
-						msg.channel.send(`<@${nate}> <:cmon:811019110531596308>`);
+						cmonDetected(msg);
 					}
 					return;
 				}
@@ -34,49 +31,28 @@ export default async function (client: Client) {
 		if (msg.content.match(/:\w*cmon\w*:/)) {
 			const status = await db.findOne({ _id: 'nate_enable' });
 			if (status && status.value === true) {
-				const time = new Date();
-
-				if (lastTime) {
-					// If this cmon was within 60 seconds of the last one
-					if ((time.getTime() - 60000) < lastTime.getTime()) {
-						msg.channel.send(`<@${nate}> <:cmon:811019110531596308>`);
-					}
-				}
-
-				// store time in memory
-				lastTime = time;
+				cmonDetected(msg);
 			}
 			return;
 		}
-
-		// const child = child_process.spawn(`curl www.google.com`);
-		// child.stdout.on('data', a => {
-		// 	console.log(a)
-		// })
-
-		// const emojis = msg.content.matchAll(/<:\w*:([0-9]*)>/g);
-		// for (const match of emojis) {
-		// 	const eID = match[1];
-		// 	const url = `https://cdn.discordapp.com/emojis/${eID}.png?v=1`;
-
-		// 	console.log(url);
-		// 	https.request(url, (r) => {
-		// 		console.log(r);
-		// 	});
-
-		// 	// http.request({
-		// 	// 	host: 'cdn.discordapp.com',
-		// 	// 	port: 443,
-		// 	// 	method: 'GET',
-		// 	// 	path: `/emojis/${eID}.png?v=1`
-		// 	// }, (res) => {
-		// 	// 	console.log(res);
-		// 	// });
-
-		// 	// const hash = crypto.createHash('sha1');
-		// 	// hash.setEncoding('hex');
-		// }
 	});
 
 	console.info('Initialized Nate listener');
+}
+
+let lastTime: Date;
+
+async function cmonDetected(msg: Message) {
+	const time = new Date();
+
+	if (lastTime) {
+		// If this cmon was within 60 seconds of the last one
+		if ((time.getTime() - 60000) < lastTime.getTime()) {
+			msg.channel.send(`<@${nate}> <:cmon:811019110531596308>`);
+			// console.log(`replied a cmon. time = ${time}, lastTime = ${lastTime}`)
+		}
+	}
+
+	// store time in memory
+	lastTime = time;
 }
