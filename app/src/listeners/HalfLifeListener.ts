d6f283dd8fd8d56785299517b44fbbfd759db044
Mudbill <mudbill.b@gmail.com>
2021-03-01 16:12:03 +0100

    HL

--- app/src/listeners/HalfLifeListener.ts
+++ app/src/listeners/HalfLifeListener.ts
@@ -4,7 +4,9 @@ import { console } from '../util/log';
 export default async function (client: Client) {
 	client.on('message', async (msg) => {
 		const txt = msg.content.toLowerCase();
-		if (txt.includes('play') && txt.includes('half') && txt.includes('life')) {
+		if (txt.includes('play')
+			&& ((txt.includes('half') && txt.includes('life'))
+				|| txt.includes('hl'))) {
 			msg.channel.send(`<:cmon:811019110531596308>`);
 		}
 	});
