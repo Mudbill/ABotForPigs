f94e1c531284d28fa0d598e55a0bfd6db57e8755
Magnus Bull <magnus@norkred.no>
2023-12-22 23:22:03 +0100

    fixed announcement channel for emoji changes

--- src/listeners/EmojiChangeListener.ts
+++ src/listeners/EmojiChangeListener.ts
@@ -1,7 +1,7 @@
 const EmojiChangeListener: Listener = async (client) => {
   client.on("emojiDelete", (emoji) => {
-    const channel = emoji.guild.channels.cache.find(
-      (c) => c.name === "announcements"
+    const channel = emoji.guild.channels.cache.find((c) =>
+      c.name.includes("announcements")
     );
     if (channel?.isTextBased()) {
       channel.send(`Emoji ${emoji} has been nuked! ${emoji.url}`);
@@ -9,8 +9,8 @@ const EmojiChangeListener: Listener = async (client) => {
   });
 
   client.on("emojiCreate", (emoji) => {
-    const channel = emoji.guild.channels.cache.find(
-      (c) => c.name === "announcements"
+    const channel = emoji.guild.channels.cache.find((c) =>
+      c.name.includes("announcements")
     );
     if (channel?.isTextBased()) {
       channel.send(`Introducing emoji ${emoji}`);
